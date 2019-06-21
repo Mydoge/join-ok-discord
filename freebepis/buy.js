26fdd574dbcb8cd6a6e751f9ca4fad3f1c5f8d9f
Mydoge <LoganLMB2008@hotmail.com>
2019-06-21 01:03:10 -0400

    Create buy.js

---
+++ freebepis/buy.js
@@ -0,0 +1,6 @@
+document.write('<a>pissing on bepis</a>')
+document.write('<title>Buying Bepis...</title>')
+wait(4000);
+document.write('<a>Adding bepis...</a>')
+wait(2000);
+window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
